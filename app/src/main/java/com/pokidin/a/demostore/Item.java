8bd185f96a7f1aacd842f577627b5a18ccac18e2
AlexanderPokidin <a.pokidin@gmail.com>
2018-11-03 18:12:27 +0200

    Initial commit

---
+++ app/src/main/java/com/pokidin/a/demostore/Item.java
@@ -0,0 +1,11 @@
+package com.pokidin.a.demostore;
+
+public class Item {
+    private String title; // "title"
+    private String price; // "price"
+    private String description; // "description"
+    private String imageUrl;
+    private String category; // "category_path"
+
+
+}
